8f0cb657b19446792083f03f75d5693daa09b8c1
Marcel Magbual <magbualmarcel@gmail.com>
2024-04-12 18:04:11 +0800

    Integrated Prisma ORM and CockroachDB

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
 import express from "express";
+import * as dotenv from "dotenv";
+import { db } from "./db/db.server";
+
+dotenv.config();
+
+if (!process.env.PORT) process.exit(1);
 
 const app = express();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Currently listening on PORT: ${PORT}`);
