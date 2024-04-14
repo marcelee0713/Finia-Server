a3e3a87c399ad1c12071a19689e39f2adde282b0
Marcel Magbual <magbualmarcel@gmail.com>
2024-04-14 22:32:19 +0800

    Added Redis

---
+++ src/db/redis.ts
@@ -0,0 +1,17 @@
+import { createClient } from "redis";
+
+type RedisClientType = ReturnType<typeof createClient>;
+
+export async function redisClient(): Promise<RedisClientType> {
+  const redis: RedisClientType = createClient({
+    password: process.env.REDIS_SECRETKEY,
+    socket: {
+      host: process.env.REDIS_HOST,
+      port: parseInt(process.env.REDIS_PORT as string),
+    },
+  });
+
+  await redis.connect();
+
+  return redis;
+}
