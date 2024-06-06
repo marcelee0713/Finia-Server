bb475631758f0ae695057159dccd1b8a32f1494e
Marcel Magbual <magbualmarcel@gmail.com>
2024-06-06 20:33:17 +0800

    Changed the database to postgres instead and added different environment scripts

--- prisma/migrations/20240508133941_transaction_cascade_delete/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE "transactions" DROP CONSTRAINT "transactions_user_id_fkey";
-
--- AddForeignKey
-ALTER TABLE "transactions" ADD CONSTRAINT "transactions_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "user"("uid") ON DELETE CASCADE ON UPDATE CASCADE;
--- prisma/migrations/20240510030342_amount_data_type/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - Changed the type of `amount` on the `transactions` table. No cast exists, the column would be dropped and recreated, which cannot be done if there is data, since the column is required.
-
-*/
--- AlterTable
-ALTER TABLE "transactions" DROP COLUMN "amount";
-ALTER TABLE "transactions" ADD COLUMN     "amount" DECIMAL(12,2) NOT NULL;
